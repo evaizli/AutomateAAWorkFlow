4e174c220b7281453a35a1d0d18f64a0da3faa36
evaizli <everrich2018@gmail.com>
2019-09-17 17:09:51 -0700

    final debug hackerrank

--- hackerRankInvite.js
+++ hackerRankInvite.js
@@ -8,7 +8,7 @@ const password = require("./personal/password").hackerPassword;
   await page.setViewport({ width: 1280, height: 700 });
 
   //CANDIDATE INFORMATION: FILL IN CANDIDATE INFORMATION BEFORE RUNNING THE SCRIPT
-  const candidateEmail = "eva.li.pan";
+  const candidateEmail = "eva.li.pan@gmail.com";
   const candidateName = "Eva Pan";
   const interviewType = "App Academy Technical Interview -" + candidateName;
   const meetingTime = "11:00";
@@ -18,23 +18,18 @@ const password = require("./personal/password").hackerPassword;
   await page.waitFor("#input-1");
   await page.waitFor(".login-form__button");
   await page.click("#input-1");
-  await page.keyboard.press("ArrowRight");
-  await page.keyboard.press("Backspace");
-  await page.type("#input-1", email, { delay: 100 });
+  await page.focus("#input-1");
+  await page.keyboard.type(email, { delay: 100 });
   await page.waitFor(1000);
-
-  // await page.$eval("#input-1", el => (el.value = window.email));
-
-  // await page.evaluate(() => {
-  //   window.email = null;
-  // });
 
   await page.waitFor(".login-form__button");
   await page.click(".login-form__button");
-
+  await page.waitFor(2000);
   await page.waitFor("#input-1");
   await page.click("#input-1");
-  await page.type("#input-1", password, { delay: 100 });
+  await page.focus("#input-1");
+  await page.keyboard.type(password, { delay: 100 });
+  await page.waitFor(1000);
   await page.click(".login-form__button");
 
   // await page.waitFor("#password");
@@ -60,122 +55,122 @@ const password = require("./personal/password").hackerPassword;
 
   // await page.click(".signupBtn");
 
-  // await page.waitFor(".tab-link");
-  // await page.goto("https://www.hackerrank.com/x/interviews/mypads");
-  // await page.waitFor(".js-new-interview");
-  // await page.click(".js-new-interview");
-  // await page.waitFor(".fw");
+  await page.waitFor(".tab-link");
+  await page.goto("https://www.hackerrank.com/x/interviews/mypads");
+  await page.waitFor(".js-new-interview");
+  await page.click(".js-new-interview");
+  await page.waitFor(".fw");
 
-  // // add candidate info and email for the invite
-  // await page.evaluate(
-  //   (candidateEmail, interviewType) => {
-  //     let inputs = document.querySelectorAll(".fw");
-  //     console.log("INPUTS", inputs);
-  //     inputs[0].value = candidateEmail;
-  //     inputs[1].value = interviewType;
-  //     inputs[2].click();
-  //   },
-  //   candidateEmail,
-  //   interviewType
-  // );
+  // add candidate info and email for the invite
+  await page.evaluate(
+    (candidateEmail, interviewType) => {
+      let inputs = document.querySelectorAll(".fw");
+      console.log("INPUTS", inputs);
+      inputs[0].value = candidateEmail;
+      inputs[1].value = interviewType;
+      inputs[2].click();
+    },
+    candidateEmail,
+    interviewType
+  );
 
-  // //invite form
+  //invite form
 
-  // await page.waitFor(".js-invite-participants");
-  // await page.click(".js-invite-participants");
+  await page.waitFor(".js-invite-participants");
+  await page.click(".js-invite-participants");
 
-  // await page.waitFor("#interview-link");
-  // const codePairLink = await page.evaluate(() => {
-  //   return document.getElementById("interview-link").href;
-  // });
-  // // open new page & set up codepair environment
-  // const page2 = await browser.newPage();
-  // await page2.setViewport({ width: 1280, height: 700 });
-  // await page2.goto(codePairLink);
+  await page.waitFor("#interview-link");
+  const codePairLink = await page.evaluate(() => {
+    return document.getElementById("interview-link").href;
+  });
+  // open new page & set up codepair environment
+  const page2 = await browser.newPage();
+  await page2.setViewport({ width: 1280, height: 700 });
+  await page2.goto(codePairLink);
 
-  // await page2.waitFor(".icon-menu-small");
-  // await page2.evaluate(() => {
-  //   let sideBar = document.querySelector(".icon-menu-small.pull-right");
-  //   sideBar.click();
-  // });
+  await page2.waitFor(".icon-menu-small");
+  await page2.evaluate(() => {
+    let sideBar = document.querySelector(".icon-menu-small.pull-right");
+    sideBar.click();
+  });
 
-  // //expand the hidden import question
-  // await page2.waitFor(".js-open-library");
+  //expand the hidden import question
+  await page2.waitFor(".js-open-library");
 
-  // await page2.evaluate(() => {
-  //   let openPrompts = document.querySelector(".js-open-library");
-  //   openPrompts.click();
-  // });
+  await page2.evaluate(() => {
+    let openPrompts = document.querySelector(".js-open-library");
+    openPrompts.click();
+  });
 
-  // //search and select instruction from the modal
-  // await page2.waitFor(".close");
+  //search and select instruction from the modal
+  await page2.waitFor(".close");
 
-  // await page2.evaluate(() => {
-  //   let instructionSearch = document.getElementsByName("searchKey")[0];
-  //   instructionSearch.value = "Tech Interview Instructions";
-  //   let searchButton = document.querySelector(".js-search-submit");
-  //   searchButton.click();
-  // });
+  await page2.evaluate(() => {
+    let instructionSearch = document.getElementsByName("searchKey")[0];
+    instructionSearch.value = "Tech Interview Instructions";
+    let searchButton = document.querySelector(".js-search-submit");
+    searchButton.click();
+  });
 
-  // await page2.waitFor(".js-use-question");
+  await page2.waitFor(".js-use-question");
 
-  // await page2.evaluate(() => {
-  //   let useQuestion = document.querySelector(".js-use-question");
-  //   useQuestion.click();
-  // });
+  await page2.evaluate(() => {
+    let useQuestion = document.querySelector(".js-use-question");
+    useQuestion.click();
+  });
 
-  // //return the main page and close page2
-  // await page2.waitFor(".icon-menu-small");
-  // await page2.close();
+  //return the main page and close page2
+  await page2.waitFor(".icon-menu-small");
+  await page2.close();
 
-  // await page.goForward();
-  // await page.waitFor("#interview-link");
+  await page.goForward();
+  await page.waitFor("#interview-link");
 
-  // const inviteURL = await page.evaluate(() => {
-  //   let url = document.URL;
-  //   return url;
-  // });
+  const inviteURL = await page.evaluate(() => {
+    let url = document.URL;
+    return url;
+  });
 
-  // await page.goto(inviteURL);
-  // await page.waitFor("#interview-link");
+  await page.goto(inviteURL);
+  await page.waitFor("#interview-link");
 
-  // await page.waitFor("#candidate-email");
+  await page.waitFor("#candidate-email");
 
-  // await page.evaluate(candidateName => {
-  //   const nameElem = document.getElementsByName("candidate-name")[0];
-  //   nameElem.value = candidateName;
-  // }, candidateName);
+  await page.evaluate(candidateName => {
+    const nameElem = document.getElementsByName("candidate-name")[0];
+    nameElem.value = candidateName;
+  }, candidateName);
 
-  // await page.waitFor("#start-date");
+  await page.waitFor("#start-date");
 
-  // await page.evaluate(() => {
-  //   const dateElem = document.getElementById("start-date");
-  //   const today = new Date();
-  //   const year = today.getFullYear();
-  //   const day = today.getDate();
-  //   const month = today.getMonth() + 1;
-  //   dateElem.value = month + "-" + day + "-" + year;
-  // });
+  await page.evaluate(() => {
+    const dateElem = document.getElementById("start-date");
+    const today = new Date();
+    const year = today.getFullYear();
+    const day = today.getDate();
+    const month = today.getMonth() + 1;
+    dateElem.value = month + "-" + day + "-" + year;
+  });
 
-  // await page.evaluate(meetingTime => {
-  //   const timeElem = document.getElementById("start-time");
-  //   timeElem.value = meetingTime;
-  // }, meetingTime);
+  await page.evaluate(meetingTime => {
+    const timeElem = document.getElementById("start-time");
+    timeElem.value = meetingTime;
+  }, meetingTime);
 
-  //UNCOMMENT TO ACTIVE COMMAND. SHOULD ALWAYS DOUBLE CHECK MEETING TIME BEFORE SEND INVITE.
+  // UNCOMMENT TO ACTIVE COMMAND. SHOULD ALWAYS DOUBLE CHECK MEETING TIME BEFORE SEND INVITE.
 
   // await page.click("#interview-update");
 
   // await page.waitFor(".hr-dialog-button");
   // await page.waitFor(".btn-primary");
 
-  //DO NOT SEND invites to all participants
+  // DO NOT SEND invites to all participants
   // await page.evaluate(() => {
   //   const doNotSend = document.querySelector(".hr-dialog-button");
   //   doNotSend.click();
   // });
 
-  //SEND invites to all participants
+  // SEND invites to all participants
   // await page.evaluate(() => {
   //   const sendInvite = document.querySelector(".btn-primary");
   //   sendInvite.click();
